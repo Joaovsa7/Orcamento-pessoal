8c72c75fe81ec0847770d21f0c14d994ddbf18b8
joaovsa7 <jvsadesign@gmail.com>
2018-10-06 21:09:04 -0300

    estrutura no local storage

---
+++ App/src/app.js
@@ -0,0 +1,38 @@
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor){
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
+
+class Bd {
+    function salvar(d){
+        localStorage.setItem('despesa', JSON.stringify(d))
+    }
+}
+
+
+function cadastrarDespesa(){
+
+    let ano = document.querySelector('#ano')
+    let mes = document.querySelector('#mes')
+    let dia = document.querySelector('#dia')
+    let tipo = document.querySelector('#tipo')
+    let descricao = document.querySelector('#descricao')
+    let valor = document.querySelector('#valor')
+
+    let despesa = new  Despesa(
+        ano.value,
+        mes.value,
+        dia.value,
+        tipo.value,
+        descricao.value,
+        valor.value
+    )
+
+    salvar(despesa)    
+}
